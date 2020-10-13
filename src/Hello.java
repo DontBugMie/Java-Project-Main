b262ca7e8055b6a699dff76fcb49c5187a702850
Maya Wright <68424579+DontBugMie@users.noreply.github.com>
2020-10-13 23:01:34 +0100

    Add files via upload

---
+++ src/Hello.java
@@ -0,0 +1,29 @@
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("Maya's first line of Java" +
+                " " +
+                "hello"
+                + " " + "Hello people!");
+
+        int myFirstNumber = 20;
+        int myAge = myFirstNumber +4;
+        int myDoorNumber = (20+4)/2+11;
+        System.out.println(myFirstNumber + "Maya");
+        System.out.println(myFirstNumber);
+        System.out.println(myAge);
+        System.out.println(myDoorNumber);
+
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+        System.out.println(myTotal);
+        System.out.println(myFirstNumber + mySecondNumber + myThirdNumber);
+        System.out.println(myFirstNumber + " " + mySecondNumber + " " +myThirdNumber);
+        System.out.println(mySecondNumber * myDoorNumber);
+        System.out.println(myLastOne);
+        System.out.println(myLastOne);
+    }
+}
